7eedfe62a628c9d0b69fe1a1a7e2f5568a5fa3a8
John Dorlus <jdorlus@outlook.com>
2014-03-27 16:55:52 -0400

    Added the royalslider and added it to the UI. Creating slider. Added functions for making sure that categories are unique and added it to the .

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -5,13 +5,48 @@ module.controller('IndexController', ['$scope', 'ProjectService',
     function($scope, ProjectService){
         var projectGet = function(data){
             $scope.projects = data.projects;
+            var categories = [];
+            categories = getUniqueJsonTrait(data.projects, categories);
+            $scope.categories = categories;
         };
         ProjectService.getAllProjects(projectGet);
     }
 ]);
 
-module.controller('TestController2', ['$scope', 'TestService2',
-    function($scope, TestService2){
+module.controller('BlogController', ['$scope', 'PostService',
+    function($scope, PostService){
 
     }
 ]);
+
+function getUniqueJsonTrait(json, resultSet) {
+    var resultTypeName , typeName;
+    for (var i in json) {
+        if(resultSet.length === 0){
+            resultSet[0] = json[i].projectType.name;
+            console.log( resultSet[0]);
+        }
+        for(var j = 0; j < resultSet.length; j++)
+        {
+            typeName = json[i].projectType.name
+            resultTypeName = resultSet[j];
+            if(!checkIfExistsInArray()stsInArray(resultSet,typeName)){
+                console.log('Hey, should skip now because ' + typeName + ' is the same as ' + resultTypeName);
+            }
+            else{
+                resultSet[resultSet.length] = json[i].projectType.name;
+            }
+
+        }
+
+    }
+    return resultSet;
+}
+function checkIfExistsInArray(array,stringToCheck){
+    for(var i in array){
+        if (array[i] == stringToCheck){
+            return false;
+        }
+    }
+    return true;
+}
