fb19f5843d6e53efb3886a57ee088beeb68d1de5
John Dorlus <jsdorlus@elastic.co>
2018-06-03 21:14:23 -0400

    Added axios to get data.

--- src/feed/feed.js
+++ src/feed/feed.js
@@ -1,6 +1,7 @@
 import React , { Component } from 'react';
 import { Grid, Card, CardContent, CardHeader, Avatar, IconButton } from '@material-ui/core';
 import { Web } from 'mdi-material-ui';
+import { Get } from 'react-axios';
 import  FeedListingItem  from './feed_listing'
 
 const styles = {
@@ -12,6 +13,23 @@ const styles = {
 }
 
 export default class Feed extends Component {
+  state = {
+    loading: false,
+    success: false,
+    error: false
+  }
+  renderResponse(error, response, isLoading, feedListingItemData) {
+    if(error) {
+      return (<div>Something bad happened: {error.message}</div>)
+    } else if(isLoading) {
+      return (<div className="loader"></div>)
+    } else if(response !== null) {
+      return (
+        <FeedListingItem listingData={response.data} /> 
+      )
+    }
+    return null
+  }
   render() {
     const feedData = [this.props.data];
     return (
@@ -36,12 +54,18 @@ export default class Feed extends Component {
                 }>
                 </CardHeader>
               <CardContent>
-                  <FeedListingItem listingData={feedListingItemData} />  
+                <Get url={feedListingItemData.requestUrl}>
+                  {this.renderResponse()} 
+                </Get>
               </CardContent>
             </Card>
         ))
       }
       </Grid>
     )
+    
+  }
+  onComponentDidMount() {
+
   }
 }
